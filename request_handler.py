238b00f9a5ef4bd2b928d1e3ab098b7af6d30a1e
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-12 14:30:28 -0500

    defined create_animal function

--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ from animals import get_all_animals, get_single_animal
 from locations import get_all_locations, get_single_location
 from employees import get_all_employees, get_single_employee
 from customers import get_all_customers, get_single_customer
-
+import json
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
 # work together for a common purpose. In this case, that
@@ -110,8 +110,20 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+        # Initialize new animal
+        new_animal = None
+         # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
