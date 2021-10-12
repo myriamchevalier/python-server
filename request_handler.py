0883361e1610841ddf462c27849af9d9e4c6c4dd
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-12 15:30:18 -0500

    defined create_customer, imported in package and request_handler.py

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
-from employees.request import create_employee
 from locations import get_all_locations, get_single_location, create_location
 from employees import get_all_employees, get_single_employee, create_employee
-from customers import get_all_customers, get_single_customer
+from customers import get_all_customers, get_single_customer, create_customer
 import json
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -119,7 +118,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         new_animal = None
         new_location = None
         new_employee = None
-        # new_customer = None
+        new_customer = None
          # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
@@ -138,6 +137,11 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_employee = create_employee(post_body)
 
             self.wfile.write(f"{new_employee}".encode())
+
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+            self.wfile.write(f"{new_customer}".encode())
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
 
