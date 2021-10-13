99c9fb309f3995a9873d31acda069922bc666f6c
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-13 15:00:12 -0500

    added PUT method in request_handler.py

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
-from locations import get_all_locations, get_single_location, create_location, delete_location
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer
+from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 import json
 
 # Here's a class. It inherits from another class.
@@ -160,6 +160,12 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Update a single animal from the list
         if resource == "animals":
             update_animal(id, post_body)
+        if resource == "locations":
+            update_location(id, post_body)
+        if resource == "employees":
+            update_employee(id, post_body)
+        if resource == "customers":
+            update_customer(id, post_body)
 
         self.wfile.write("".encode())
 
