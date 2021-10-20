337ae9144c27ef3a2e726109c77e8976f2404f54
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-20 12:10:32 -0500

    implemented code to extend info on employees

--- employees/request.py
+++ employees/request.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 from models import Employee
+from models.location import Location
 
 EMPLOYEES = [
     {
@@ -40,8 +41,12 @@ def get_all_employees() :
             e.id,
             e.name,
             e.address,
-            e.location_id
+            e.location_id,
+            l.name location_name,
+            l.address location_address
         FROM employee e
+        JOIN location l
+            ON l.id = e.location_id
         """)
 
         employees = []
@@ -50,6 +55,9 @@ def get_all_employees() :
 
         for row in dataset:
             employee = Employee(row['id'], row['name'], row['address'], row['location_id'])
+
+            location = Location(row['id'], row['location_name'], row['location_address'])
+            employee.location = location.__dict__
             employees.append(employee.__dict__)
     
     return json.dumps(employees)
