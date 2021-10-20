1ef9f7ab268d739cd9384a381513e8e937b64a5a
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-20 14:18:57 -0500

    implemented code to create new animal in db

--- animals/request.py
+++ animals/request.py
@@ -76,9 +76,11 @@ def get_all_animals():
                             row['status'], row['location_id'],
                             row['customer_id'])
 
-            location = Location(row['id'], row['location_name'], row['location_address'])
+            location = Location(
+                row['id'], row['location_name'], row['location_address'])
 
-            customer = Customer(row['id'], row['customer_name'], row['customer_email'])
+            customer = Customer(
+                row['id'], row['customer_name'], row['customer_email'])
 
             animal.location = location.__dict__
             animal.customer = customer.__dict__
@@ -120,21 +122,30 @@ def get_single_animal(id):
         return json.dumps(animal.__dict__)
 
 
-def create_animal(animal):
-    # Get the id value of the last animal in the list
-    max_id = ANIMALS[-1]["id"]
+def create_animal(new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-    # Add 1 to whatever that number is
-    new_id = max_id + 1
+        db_cursor.execute("""
+        INSERT INTO animal
+            ( name, breed, status, location_id, customer_id)
+        VALUES
+            ( ?, ?, ?, ?, ?)
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'],))
 
-    # Add an 'id' property to the animal dictionary
-    animal["id"] = new_id
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
 
-    # Add the animal dictionary to the list
-    ANIMALS.append(animal)
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_animal['id'] = id
 
-    # Return the dictionary with 'id' property added
-    return animal
+    return json.dumps(new_animal)
 
 
 def delete_animal(id):
